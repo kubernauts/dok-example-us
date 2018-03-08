1e66e9c7c4dff8a179342787324387c6e5096830
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-03-08 17:05:15 +0000

    completes stock-gen MVP

--- stock-gen/main.go
+++ stock-gen/main.go
@@ -1,28 +1,33 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
+	"io"
+	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 // Stock represents a company listed at a stock market.
 type Stock struct {
 	Symbol   string  `json:"symbol"`
-	Value    float32 `json:"current"`
+	Value    float64 `json:"value"`
 	Currency string  `json:"currency"`
 }
 
 func main() {
-	dummy := Stock{
-		Symbol:   "GOOG",
-		Value:    100,
-		Currency: "USD",
+	stocks, err := loadsym()
+	if err != nil {
+		fmt.Printf("Couldn't load symbols due to %v\n", err)
 	}
 	http.HandleFunc("/stockdata", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		err := json.NewEncoder(w).Encode(dummy)
+		idx := rand.Intn(len(stocks))
+		err = json.NewEncoder(w).Encode(stocks[idx])
 		if err != nil {
 			fmt.Printf("HTTP %v\n", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -32,8 +37,37 @@ func main() {
 	if port == "" {
 		port = "80"
 	}
-	err := http.ListenAndServe(":"+port, nil)
+	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		fmt.Printf("Error launching service %v\n", err)
 	}
+}
+
+func loadsym() (stocks []Stock, err error) {
+	symfile := os.Getenv("DOK_STOCKGEN_SYMBOLS")
+	if symfile == "" {
+		symfile = "./symbols.csv"
+	}
+	c, err := os.Open(symfile)
+	if err != nil {
+		return stocks, err
+	}
+	for {
+		record, error := csv.NewReader(bufio.NewReader(c)).Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			return stocks, err
+		}
+		v, err := strconv.ParseFloat(record[1], 64)
+		if err != nil {
+			return stocks, err
+		}
+		stocks = append(stocks, Stock{
+			Symbol:   record[0],
+			Value:    v,
+			Currency: "USD",
+		})
+	}
+	return stocks, nil
 }
